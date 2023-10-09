c5d7e00b21c774eb4243d743bce2af3945360a4b
Jose ortiz <109627243+ojose09@users.noreply.github.com>
2023-10-09 00:48:22 -0500

    .

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -1,37 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float walkSpeed = 2.0f;
-    public float runSpeed = 6.0f;
+    public float moveSpeed = 5.0f;
+    public float runMultiplier = 2.0f;
+    public float rotationSpeed = 10.0f;
+    public float speedSmoothTime = 0.1f; // Tiempo de suavizado para la velocidad
 
     private Animator animator;
+    private CharacterController controller;
+    private float currentSpeed = 0.0f;
+    private float speedSmoothVelocity;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
+        // Obtén la entrada horizontal (eje X) y vertical (eje Z) del jugador.
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 inputDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
-        float speed = inputDirection.magnitude;
+        // Calcula la dirección de movimiento.
+        Vector3 moveDirection = new Vector3(horizontalInput, 0.0f, verticalInput);
+        moveDirection = transform.TransformDirection(moveDirection);
 
+        // Aplica la velocidad de movimiento.
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            // Running
-            speed = Mathf.Clamp01(speed) * runSpeed;
+            currentSpeed = moveSpeed * runMultiplier;
         }
         else
         {
-            // Walking
-            speed = Mathf.Clamp01(speed) * walkSpeed;
+            currentSpeed = moveSpeed;
         }
 
-        // Actualiza el parámetro de velocidad en el Animator
-        animator.SetFloat("Velocidad", speed);
+        Vector3 movement = moveDirection.normalized * currentSpeed;
+        controller.SimpleMove(movement);
+
+        // Actualiza el parámetro "Velocidad" en el Animator con interpolación suave.
+        float speedNormalized = Mathf.SmoothDamp(animator.GetFloat("Velocidad"), movement.magnitude / (moveSpeed * runMultiplier), ref speedSmoothVelocity, speedSmoothTime);
+        animator.SetFloat("Velocidad", speedNormalized);
+
+        // Rotación del personaje.
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            Quaternion rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = rotation;
+        }
     }
 }
